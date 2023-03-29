caa2418eaf88b350a86d99ec22ced8f734b8283b
hyoung <ycat01@naver.com>
2023-03-29 22:44:44 +0900

    Setting

---
+++ src/main/java/com/dotorder/DotOrder/DotOrderApplication.java
@@ -0,0 +1,13 @@
+package com.dotorder.DotOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotorder/DotOrder/DotOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotorder.DotOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
