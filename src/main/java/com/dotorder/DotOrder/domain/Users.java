10b1e6f2f0c0a9d78ee51abc93c34123b18cbedb
hyoung <ycat01@naver.com>
2023-04-14 16:01:50 +0900

    [FEAT] 회원가입

---
+++ src/main/java/com/dotorder/DotOrder/domain/Users.java
@@ -0,0 +1,39 @@
+package com.dotorder.DotOrder.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@DynamicInsert
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idx;
+
+    @Column(nullable = false, length = 15)
+    @Schema(description = "아이디", example = "abc")
+    private String id;
+
+    @Column(nullable = false, length = 20)
+    @Schema(description = "비밀번호", example = "a1234")
+    private String password;
+
+    @Column(nullable = false, length = 15)
+    @Schema(description = "상태", example = "guest")
+    private String status;
+
+    @Builder
+    public Users(String id, String password, String status){
+        this.id=id;
+        this.password=password;
+        this.status=status;
+    }
+}
